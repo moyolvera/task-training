9f1502f7eb2047ead82e97c7c6d3cba58b54a2b7
Moises Olvera <moises.olvera@itexico.com>
2020-03-15 22:58:18 -0600

    Project environment setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,32 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: ['prettier', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'jest'],
+  rules: {
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': 'off',
+    'global-require': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-closing-bracket-location': 'off',
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,7 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
 
 import {
   Header,
@@ -26,16 +19,14 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-declare var global: {HermesInternal: null | {}};
+declare var global: { HermesInternal: null | {} };
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
@@ -46,8 +37,8 @@ const App = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see your
+                edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
@@ -55,6 +46,9 @@ const App = () => {
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
+            </View>
+            <View>
+              <Text>ho.o</Text>
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Debug</Text>
@@ -64,9 +58,7 @@ const App = () => {
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
+              <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
             </View>
             <LearnMoreLinks />
           </View>
